4f85e5e547f3516268228b8833923e62a628931c
William <ferruzola25@gmail.com>
2022-02-28 18:49:26 -0500

    primer commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { ContadorModule } from './contador/contador.module';
+import { HeroesModule } from './heroes/heroes.module';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    HeroesModule,
+    ContadorModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
